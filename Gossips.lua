7365af37c4f43ed61137e70f4563839b7da8cb09
depthbomb <depthbomb@users.noreply.github.com>
2024-07-29 15:46:13 -0500

    chore: improve comments

--- Gossips.lua
+++ Gossips.lua
@@ -1,22 +1,25 @@
 local _, AutoGossip = ...
 
+-[[
+This table holds an array of gossip option IDs keyed by the NPC that provides said options.
+
+Use `/dump C_GossipInfo.GetOptions()` with the gossip window open to get the option IDs.
+]]
 AutoGossip.GOSSIPS = {
 	-- Misc.
 	["Katy Stampwhistle"] = {48598},
+
 	-- Hillsbrad Foothills
 	["Dumass"] = {
-		39812, 39344, -- Quest "<todo>"
+		39812, 39344,        -- Quest "Welcome to the Machine"
+		                     -- TODO Deep Mine Rescue
 	},
 	["Kingslayer Orkus"] = {
-		39118, 39073, 39072, -- Quest "<todo>"
+		39118, 39073, 39072, -- Quest "Welcome to the Machine"
 		39520,               -- Quest "*Gurgle* HELP! *Gurgle*"
 		39079,               -- Quest "Heroes of the Horde!"
 	},
 	["Johnny Awesome"] = {
-		38308, 39120, 39119, -- Quest "<todo>"
+		38308, 39120, 39119, -- Quest "Welcome to the Machine"
 	},
 }
-
--- /dump C_GossipInfo.GetOptions()
-
-
